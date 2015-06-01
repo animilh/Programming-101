1b0a37f707372349b09a55933a93b5fe8711eb88
Ana Burnaska <ahristova.milenova@gmail.com>
2015-06-01 16:30:23 +0300

    some modification

--- week4/Music-Library/crawler.py
+++ week4/Music-Library/crawler.py
@@ -1,10 +1,10 @@
 import os
+import datetime
 import mutagen.id3
 from mutagen.mp3 import MP3
 from mutagen.easyid3 import EasyID3
 from song import Song
 from playlist import Playlist
-
 
 class Crawler:
 
@@ -13,31 +13,31 @@ class Crawler:
     def __init__(self, path):
         self.path = os.path.abspath(path)
 
-    def get_id3_tags(self, file_path):
+    def get_id3_tags(self, audio):
         result = {}
-        try:
-            audio = MP3(file_path, ID3=EasyID3)
-            result['artist'] = audio['artist'][0]
-            result['title'] = audio['title'][0]
-            result['album'] = audio['album'][0]
-            result['length'] = audio.info.length
-            return result
-        except mutagen.id3.error:
-            pass
+        result['artist'] = audio['artist'][0]
+        result['title'] = audio['title'][0]
+        result['album'] = audio['album'][0]
+        result['length'] = str(datetime.timedelta(seconds=int(audio.info.length)))
+        return result
+
 
     def generate_playlist(self):
         files_in_dir = [filename for filename in os.listdir(self.path) if filename.endswith(Crawler.file_types)]
         my_list = Playlist(name="Yohoho", repeat=False, shuffle=False)
         for music_file in files_in_dir:
-            tags = self.get_id3_tags(str(self.path + '/' + music_file))
-            my_list.add_song(Song(tags['title'], tags['artist'], tags['album'], tags['length']))
+            try:
+                audio = MP3(self.path + '/' + music_file, ID3=EasyID3)
+                tags = self.get_id3_tags(audio)
+                my_list.add_song(Song(tags['title'], tags['artist'], tags['album'], tags['length']))
+            except mutagen.id3.error:
+                continue
         return my_list
 
 def main():
     crawler = Crawler('/home/burnaski/Music')
     playlist = crawler.generate_playlist()
-    for song in playlist:
-        print('{} - {} length {}').format(song.title(), song.artist(), song.length())
+    print(playlist)
 
 if __name__ == '__main__':
     main()
