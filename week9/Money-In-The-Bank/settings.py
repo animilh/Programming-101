ac4a135ca565e53f209051e26e7feef9f2181dcf
animilh <ahristova.milenova@gmail.com>
2015-07-01 14:36:05 +0300

    Create settings.py

---
+++ week9/Money-In-The-Bank/settings.py
@@ -0,0 +1,12 @@
+DB_CONNECTION_STRING = "sqlite:///bank.db"
+
+commands_main = {'register' : 'for creating new account!',
+                 'login' : 'for logging in!', 
+				 'help' : 'for showing commands',
+				 'exit' : 'for closing program!',}
+				 
+commands_logged = {'info' : 'for showing account info',
+                   'change pass' : 'for changing password',
+				   'change message' : 'for changing user message',
+				   'show message' : 'for showing message', 
+				   'help' : 'for showing commands',}
